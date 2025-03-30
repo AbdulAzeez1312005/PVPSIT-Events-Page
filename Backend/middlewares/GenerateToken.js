f9e571e252b07c233ba42c1f6d29d01e719902ea
G. VENKATA SAI RAM <saigovvala2346@gmail.com>
2025-03-30 23:41:58 +0530

    Update GenerateToken.js

--- Backend/middlewares/GenerateToken.js
+++ Backend/middlewares/GenerateToken.js
@@ -2,15 +2,15 @@ import jwt from 'jsonwebtoken';
 
 export const generateTokenAndSetCookies = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
+        expiresIn: "7d",
     });
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000
+        secure: process.env.NODE_ENV === 'production', // Ensure cookie is secure in production (HTTPS)
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict', // Use 'None' for cross-origin requests in production
+        maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expires in 7 days
     });
 
     return token;
-};  
+};
